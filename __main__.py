cd1ad866ff1ca97935c6b63a8a191636ff5e2198
Ubuntu <ubuntu@ip-172-31-47-229.us-west-2.compute.internal>
2020-12-14 18:14:28 +0000

    Fixed bugs and added lines

--- __main__.py
+++ __main__.py
@@ -15,9 +15,9 @@ class TodoList:
             os.mkdir("logs\\" + name_lowercase)
             os.mkdir("logs\\" + name_lowercase + "\\completed\\")
         except FileExistsError:
-            print(f"Welcome back {user_name}!")
+            print(f"Welcome back {user_name}!\n")
         except (OSError, FileNotFoundError):
-            print("Impossible username")
+            print("Impossible username\n")
         """
         Changing working directory to the newly created directory.
         """
@@ -25,35 +25,40 @@ class TodoList:
 
 
 def menu():
-    menu_choice = input(("-" * 33)
-                        + "\n|\tMenu:\t\t\t\t\t\t|\n"
-                          "|\t[1]\tView todo list\t\t\t|\n"
-                          "|\t[2]\tAdd new item\t\t\t|\n"
-                          "|\t[3]\tComplete item\t\t\t|\n"
-                          "|\t[4]\tDelete item\t\t\t\t|\n"
-                          "|\t[5]\tView completed items\t|\n"
-                          "|\t[0]\tExit\t\t\t\t\t|\n"
-                        + ("-" * 33) + " \n")
+    menu_choice = input("\nMenu:\n"
+                        "[1]\tView todo list\n"
+                        "[2]\tAdd new item\n"
+                        "[3]\tComplete item\n"
+                        "[4]\tDelete item\n"
+                        "[5]\tView completed items\n"
+                        "[0]\tExit\n"
+                        "Select number:\t")
+    print()
     selection(menu_choice)
 
 
 def view_todos():
-    print(("-" * 33) + f"\n{str(user_name)}'s To-Do List.\n" + ("-" * 33))
+    print(f"\n{str(user_name)}'s To-Do List.\n")
     todo_list = glob.glob(f"{path}\\*.txt")
-    counter = 1
+    counter = 1      
     if todo_list:
         for todo in todo_list:
-            print(f"\t{counter}:\t{todo[len(path) + 1:-4]}")
-            counter += 1
+            if "completed" in todo:
+                continue
+            else:
+                print(f"{counter}:\t{todo[len(path) + 1:-4]}")
+                counter += 1
     else:
         print("No items in list")
-    print("-" * 33)
+    print()
 
 
 def add_item():
     task = input("Enter name of todo item:\n")
+    print()
     task_description = input(
         "Add a description if you like (press Enter to continue)\n")
+    print()
     try:
         if task:
             new_file = open(f"{path}\\{task}.txt", "w+")
@@ -71,9 +76,9 @@ def complete_item():
     completed_item = input("What is the name of the item you have completed?\n")
     try:
         if completed_item:
-
             Path(f"{path}\\{completed_item}.txt") \
                 .rename(f"{path}\\completed\\{completed_item}.txt")
+            print(f"\n{completed_item} was completed. Well done!")
         else:
             print("You did not enter a name for the item")
     except PermissionError:
@@ -84,21 +89,26 @@ def complete_item():
 
 def delete_item():
     item_to_delete = input("What is the name of the item you wish to delete?\n")
-    if os.path.exists(item_to_delete + ".txt"):
-        os.remove(item_to_delete + ".txt")
+    if os.path.exists(f"{path}\\{item_to_delete}.txt"):
+        os.remove(f"{path}\\{item_to_delete}.txt")
+        print()
+        print(f"{item_to_delete} has been deleted.")
+    elif os.path.exists(f"{path}\\completed\\{item_to_delete}.txt"):
+        os.remove(f"{path}\\completed\\{item_to_delete}.txt")
+        print()
         print(f"{item_to_delete} has been deleted.")
     else:
         print("File does not exist")
 
 
 def view_completed():
-    print(f"{str(user_name)}'s completed items.")
+    print(f"{str(user_name)}'s completed items.\n")
     completed_list = glob.glob(f"{path}\\completed\\*.txt")
     counter = 0
     if completed_list:
 
         for item in completed_list:
-            print(f"\t{counter + 1}:\t" + item[len(f"{path}\\completed\\"): -4])
+            print(f"{counter + 1}:\t" + item[len(f"{path}\\completed\\"): -4])
             counter += 1
     else:
         print("No completed items yet :(")
